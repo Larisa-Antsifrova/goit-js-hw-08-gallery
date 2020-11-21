f5e6ef8a6b9633887010e373d7d7e9680f0ef13b
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2020-11-21 22:02:46 +0200

    Regrouped project files. Refactored the code for the  basic tasks.

--- js/task-gallery.js
+++ js/task-gallery.js
@@ -1,51 +1,52 @@
 import images from "./gallery-items.js";
+// Базовое задание: Создать галерею с возможностью клика по ее элементам и просмотра полноразмерного изображения в модальном окне.
 
-// Получение доступа к родительскому элементу всех лишек галереи
+// Получение доступов:
+// к родительскому элементу всех лишек галереи
 const galleryContainer = document.querySelector("ul.js-gallery");
-// Получение доступа к элементу модального окна
+// к контейнеру модального окна
 const modalContainer = document.querySelector("div.lightbox");
-// Получение доступа к кнопке закрытия модалки
+// к кнопке закрытия модалки
 const modalCloseBtn = document.querySelector(
   'button[data-action="close-lightbox"]'
 );
-// Функция для создания и рендера разметки по массиву данных и предоставленному шаблону.
-function createGalleryMarkup(images) {
-  return images
+// к элементу изображения в модалке
+const modalImageElement = modalContainer.querySelector(".lightbox__image");
+
+// Функция для создания и рендера разметки по массиву данных и по предоставленному шаблону
+function createGalleryMarkup(imagesArray) {
+  return imagesArray
     .map(({ preview, original, description }) => {
       return `<li class="gallery__item">
-  <a
-    class="gallery__link"
-    href=${original}
-  >
+  <a class="gallery__link" href=${original}>
     <img
       class="gallery__image"
       src=${preview}
       data-source=${original}
-      alt=${description}
-    />
+      alt=${description}/>
   </a>
 </li>`;
     })
     .join("");
 }
 
-// Создание и рендер разметки по массиву данных и предоставленному шаблону.
+// Создание и рендер разметки по массиву данных
 const galleryMarkup = createGalleryMarkup(images);
+
 // Добавление разметки всех лишек галереи в DOM
 galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
 
-// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
-galleryContainer.addEventListener("click", openOriginalInModal);
+// Реализация делегирования на галерее ul.js-gallery
 // Функция, которая подменяет пути к оригинальным изображениям в окне модалки
 function openOriginalInModal(event) {
   event.preventDefault();
+
   if (event.target.nodeName !== "IMG") {
     return;
   }
 
   const originalImageSource = event.target.dataset.source;
   const originalImageAlt = event.target.alt;
-  const modalImageElement = modalContainer.querySelector(".lightbox__image");
 
   modalImageElement.setAttribute("src", originalImageSource);
   modalImageElement.setAttribute("alt", originalImageAlt);
@@ -55,11 +56,13 @@ function openOriginalInModal(event) {
 
 // Функция для закрытия модалки по клику на кнопку закрытия
 function closeModal() {
-  const modalImageElement = modalContainer.querySelector(".lightbox__image");
   modalImageElement.setAttribute("src", "");
   modalImageElement.setAttribute("alt", "");
   modalContainer.classList.remove("is-open");
 }
+
+// Добавление слушателя собития на контейнер галереи
+galleryContainer.addEventListener("click", openOriginalInModal);
 
 // Добавление слушателя события на кнопку закрытия модалки
 modalCloseBtn.addEventListener("click", closeModal);
