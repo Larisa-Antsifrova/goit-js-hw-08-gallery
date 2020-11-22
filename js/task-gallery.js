78162731a5f0599597ddc50ea89ec1e9e7258dd0
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2020-11-22 02:13:11 +0200

    Added variants for extra tasks.

--- js/task-gallery.js
+++ js/task-gallery.js
@@ -16,13 +16,14 @@ const modalImageElement = modalContainer.querySelector(".lightbox__image");
 // Функция для создания и рендера разметки по массиву данных и по предоставленному шаблону
 function createGalleryMarkup(imagesArray) {
   return imagesArray
-    .map(({ preview, original, description }) => {
+    .map(({ preview, original, description }, index) => {
       return `<li class="gallery__item">
   <a class="gallery__link" href=${original}>
     <img
       class="gallery__image"
       src=${preview}
       data-source=${original}
+      data-index="${index}"
       alt=${description}/>
   </a>
 </li>`;
@@ -38,7 +39,7 @@ galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
 
 // Реализация делегирования на галерее ul.js-gallery
 // Функция, которая подменяет пути к оригинальным изображениям в окне модалки
-function openOriginalInModal(event) {
+function openModal(event) {
   event.preventDefault();
 
   if (event.target.nodeName !== "IMG") {
@@ -62,7 +63,7 @@ function closeModal() {
 }
 
 // Добавление слушателя собития на контейнер галереи
-galleryContainer.addEventListener("click", openOriginalInModal);
+galleryContainer.addEventListener("click", openModal);
 
 // Добавление слушателя события на кнопку закрытия модалки
 modalCloseBtn.addEventListener("click", closeModal);
@@ -87,3 +88,30 @@ function onPressEscape(event) {
 window.addEventListener("keydown", onPressEscape);
 
 // 3. Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
+// Доступ к длине коллекции изображений в галерее
+const galleryLength = document.getElementsByClassName("gallery__image").length;
+
+// Добавление слушателя события для получения индекса текущего изображения в модалке
+galleryContainer.addEventListener("click", getCurrentIndex);
+
+// Функция, которая достаёт индекс текущего изображения в модалке, а внутри понеслась!
+function getCurrentIndex(event) {
+  let currentIndex = +event.target.dataset.index;
+
+  window.addEventListener("keydown", slideImage);
+
+  function slideImage(event) {
+    if (event.key === "ArrowRight" && currentIndex < galleryLength - 1) {
+      currentIndex += 1;
+      console.log(currentIndex);
+      modalImageElement.setAttribute("src", images[currentIndex].original);
+      modalImageElement.setAttribute("alt", images[currentIndex].description);
+    }
+    if (event.key === "ArrowLeft" && currentIndex > 0) {
+      currentIndex -= 1;
+      console.log(currentIndex);
+      modalImageElement.setAttribute("src", images[currentIndex].original);
+      modalImageElement.setAttribute("alt", images[currentIndex].description);
+    }
+  }
+}
